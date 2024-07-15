238d1e440feed394784961f76f3604e0880c2aa0
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-07-15 19:10:29 +0300

    Delete Skill is over

--- MvcCV/Controllers/SkillController.cs
+++ MvcCV/Controllers/SkillController.cs
@@ -5,26 +5,38 @@ using System.Web;
 using System.Web.Mvc;
 using MvcCV.Models.Entity;
 using MvcCV.Repositories;
-namespace MvcCV.Controllers{
+
+namespace MvcCV.Controllers
+{
     public class SkillController : Controller
     {
         // GET: Skill
-
         GenericRepository<TblSkills> repo = new GenericRepository<TblSkills>();
+
         public ActionResult Index()
         {
             var skills = repo.List();
             return View(skills);
         }
-        [HttpGet]
-        public ActionResult newSkill()
-        {
 
+        [HttpGet]
+        public ActionResult yeniskill()
+        {
             return View();
         }
+
         [HttpPost]
-        public ActionResult newSkill(TblSkills p)
+        public ActionResult yeniskill(TblSkills p)
         {
             repo.TAdd(p);
             return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteSkill(int id)
+        {
+            var skill= repo.Find(x=>x.id==id);
+            repo.TRemove(skill);
+            return RedirectToAction("Index");   
+        }
     }
+}
