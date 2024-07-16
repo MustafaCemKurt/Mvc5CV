a64066dc3486327c97153a1646030dcdc8fc1da4
Mustafa Ck <mustafacemkrt012@gmail.com>
2024-07-16 17:45:31 +0300

    Certificates Part1 is over

---
+++ MvcCV/Controllers/CertificateController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCV.Models.Entity;
+using MvcCV.Repositories;
+namespace MvcCV.Controllers
+{
+    public class CertificateController : Controller
+    {
+        // GET: Certificate
+
+        GenericRepository<TblCertificiates> repo= new GenericRepository<TblCertificiates>();
+        public ActionResult Index()
+        {
+            var certi = repo.List();
+            return View(certi);
+        }
+        [HttpGet]
+        public ActionResult GetCerti(int id)
+        {
+            var certi = repo.Find(x=>x.id == id);
+            return View(certi);
+        }
+        [HttpPost]
+
+        public ActionResult GetCerti(TblCertificiates t)
+        {
+            var certi = repo.Find(x => x.id == t.id);
+            certi.organizator = t.organizator;
+            certi.comment = t.comment;
+            certi.date = t.date;
+            repo.TUpdate(certi);
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult NewCerti()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+
+        public ActionResult NewCerti(TblCertificiates p)
+        {
+            repo.TAdd(p);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
